6fa1674ca3f3ad6caa43b0087a1d03eafff64582
S <svs7676@psu.edu>
2025-11-30 23:18:51 -0500

    web-dev-server.config.mjs

--- web-dev-server.config.mjs
+++ web-dev-server.config.mjs
@@ -8,20 +8,24 @@ export default /** @type {import('@web/dev-server').DevServerConfig} */ ({
   watch: !hmr,
   https: true,
   dedupe: true,
+
   /** Resolve bare module imports */
   nodeResolve: {
     exportConditions: ['browser', 'development'],
   },
-  
+
   /** Compile JS for older browsers. Requires @web/dev-server-esbuild plugin */
   // esbuildTarget: 'auto'
 
   /** Set appIndex to enable SPA routing */
-  // appIndex: 'demo/index.html',
+  appIndex: 'index.html',
 
   plugins: [
     /** Use Hot Module Replacement by uncommenting. Requires @open-wc/dev-server-hmr plugin */
-    // hmr && hmrPlugin({ exclude: ['**/*/node_modules/**/*'], presets: [presets.litElement] }),
+    // hmr && hmrPlugin({
+    //   exclude: ['**/*/node_modules/**/*'],
+    //   presets: [presets.litElement]
+    // }),
   ],
 
   // See documentation for all available options
