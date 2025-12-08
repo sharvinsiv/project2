9aec9b3a1b4ebe711eaa5614908a12f12af64685
S <svs7676@psu.edu>
2025-12-07 19:45:56 -0500

    rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,20 @@
 import resolve from '@rollup/plugin-node-resolve';
 import esbuild from 'rollup-plugin-esbuild';
 import html from '@web/rollup-plugin-html';
-import { terser } from '@rollup/plugin-terser';
 import copy from 'rollup-plugin-copy';
 
 export default {
   input: 'src/project2-app.js',
   output: {
     dir: 'public',
-    format: 'es',
-    sourcemap: true
+    format: 'es'
   },
   plugins: [
     resolve(),
     esbuild({ target: 'es2017' }),
-    html({ minify: true }),
-    terser(),
+    html(),
     copy({
-      targets: [
-        { src: 'src/index.html', dest: 'public/' }
-      ],
-      verbose: true,
+      targets: [{ src: 'src/index.html', dest: 'public/' }],
       copyOnce: true
     })
   ],
