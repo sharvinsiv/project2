5c2efbf602bb0a4df0250504bdd57fb05725f5da
S <svs7676@psu.edu>
2025-12-07 21:14:31 -0500

    rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,52 +1,38 @@
-import nodeResolve from '@rollup/plugin-node-resolve';
-import babel from '@rollup/plugin-babel';
 import { rollupPluginHTML as html } from '@web/rollup-plugin-html';
 import { importMetaAssets } from '@web/rollup-plugin-import-meta-assets';
+import nodeResolve from '@rollup/plugin-node-resolve';
 import esbuild from 'rollup-plugin-esbuild';
-import pkg from 'rollup-plugin-copy';
-const copy = pkg;
+import babel from '@rollup/plugin-babel';
+import copyPlugin from 'rollup-plugin-copy';
 
 export default {
   input: 'index.html',
   output: {
+    dir: 'public',
+    format: 'es',
     entryFileNames: '[hash].js',
     chunkFileNames: '[hash].js',
-    assetFileNames: '[hash][extname]',
-    format: 'es',
-    dir: 'public',
+    assetFileNames: '[hash][extname]'
   },
   preserveEntrySignatures: false,
   plugins: [
     html({ minify: true }),
-    copy({
-      targets: [
-        { src: 'assets', dest: 'public/', flatten: false },
-      ],
+    copyPlugin({
+      targets: [{ src: 'assets', dest: 'public/' }]
     }),
     nodeResolve(),
-    esbuild({
-      minify: true,
-      target: ['chrome64', 'firefox67', 'safari11.1'],
-    }),
+    esbuild({ minify: true, target: ['chrome64', 'firefox67', 'safari11.1'] }),
     importMetaAssets(),
     babel({
+      babelHelpers: 'bundled',
       plugins: [
-        [
-          'babel-plugin-template-html-minifier',
-          {
-            modules: { lit: ['html', { name: 'css', encapsulation: 'style' }] },
-            failOnError: false,
-            strictCSS: true,
-            htmlMinifier: {
-              collapseWhitespace: true,
-              conservativeCollapse: true,
-              removeComments: true,
-              caseSensitive: true,
-              minifyCSS: true,
-            },
-          },
-        ],
-      ],
-    }),
-  ],
+        ['babel-plugin-template-html-minifier', {
+          modules: { lit: ['html', { name: 'css', encapsulation: 'style' }] },
+          failOnError: false,
+          strictCSS: true,
+          htmlMinifier: { collapseWhitespace: true, removeComments: true, minifyCSS: true }
+        }]
+      ]
+    })
+  ]
 };
