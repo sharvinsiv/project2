646557b1ed459312d766c5e2188047ae682f79ba
S <svs7676@psu.edu>
2025-12-07 21:33:41 -0500

    rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,10 @@
+import nodeResolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
 import { rollupPluginHTML as html } from '@web/rollup-plugin-html';
 import { importMetaAssets } from '@web/rollup-plugin-import-meta-assets';
-import nodeResolve from '@rollup/plugin-node-resolve';
 import esbuild from 'rollup-plugin-esbuild';
-import babel from '@rollup/plugin-babel';
-import copyPlugin from 'rollup-plugin-copy';
+import copyPkg from 'rollup-plugin-copy';
+const copy = copyPkg;
 
 export default {
   input: 'index.html',
@@ -17,21 +18,34 @@ export default {
   preserveEntrySignatures: false,
   plugins: [
     html({ minify: true }),
-    copyPlugin({
-      targets: [{ src: 'assets', dest: 'public/' }]
+    copy({
+      targets: [
+        { src: 'assets', dest: 'public/' }
+      ]
     }),
     nodeResolve(),
-    esbuild({ minify: true, target: ['chrome64', 'firefox67', 'safari11.1'] }),
+    esbuild({
+      minify: true,
+      target: ['chrome64', 'firefox67', 'safari11.1']
+    }),
     importMetaAssets(),
     babel({
-      babelHelpers: 'bundled',
       plugins: [
-        ['babel-plugin-template-html-minifier', {
-          modules: { lit: ['html', { name: 'css', encapsulation: 'style' }] },
-          failOnError: false,
-          strictCSS: true,
-          htmlMinifier: { collapseWhitespace: true, removeComments: true, minifyCSS: true }
-        }]
+        [
+          'babel-plugin-template-html-minifier',
+          {
+            modules: { lit: ['html', { name: 'css', encapsulation: 'style' }] },
+            failOnError: false,
+            strictCSS: true,
+            htmlMinifier: {
+              collapseWhitespace: true,
+              conservativeCollapse: true,
+              removeComments: true,
+              caseSensitive: true,
+              minifyCSS: true
+            }
+          }
+        ]
       ]
     })
   ]
