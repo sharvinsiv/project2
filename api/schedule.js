2c83f91ddd32a57b1cae1a04113c6cc1a26fdf28
S <svs7676@psu.edu>
2025-12-15 14:49:41 -0500

    schedule.js

--- api/schedule.js
+++ api/schedule.js
@@ -1,22 +1,24 @@
 export default function handler(req, res) {
   res.status(200).json([
     {
-      date: "Dec 8",
       opponent: "The Philadelphia Penalties",
-      location: "Away",
-      time: "3:00 PM"
+      date: "2025-04-12",
+      time: "3:00 PM",
+      location: "Home"
     },
     {
-      date: "Dec 17",
-      opponent: "Harrisburg Handlers",
-      location: "Away",
-      time: "2:50 PM"
-    },
-    {
-      date: "Dec 26",
       opponent: "Pittsburgh Riverhounds",
-      location: "Home",
-      time: "10:30 AM"
+      date: "2025-04-19",
+      time: "6:30 PM",
+      location: "Away"
+    },
+    {
+      opponent: "Harrisburg Handlers",
+      date: "2025-04-26",
+      time: "1:00 PM",
+      location: "Home"
     }
   ]);
+}
+
 }
