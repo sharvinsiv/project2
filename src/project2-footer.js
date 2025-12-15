eea800413c33ece606a51d599ffdcc57bdd8bd71
S <svs7676@psu.edu>
2025-12-15 00:23:31 -0500

    project2-footer.js

--- src/project2-footer.js
+++ src/project2-footer.js
@@ -6,35 +6,55 @@ import { LitElement, html, css } from "lit";
 import { DDDSuper } from "@haxtheweb/d-d-d/d-d-d.js";
 
 export class Project2Footer extends DDDSuper(LitElement) {
-static get tag() { return "project2-footer"; }
+  static get tag() {
+    return "project2-footer";
+  }
 
-static styles = css`
-footer {
-background: #1f5f2a;
-padding: 24px;
-text-align: center;
-color: white;
-}
+  static get styles() {
+    return [super.styles, css`
+      footer {
+        background: #1f5f2a;
+        padding: 24px;
+        text-align: center;
+        color: white;
+      }
 
-a {
-color: #a5d6a7;
-margin: 0 10px;
-text-decoration: none;
-}
-`;
+      .social-links {
+        margin-bottom: 8px;
+      }
 
-render() {
-return html`
-<footer>
-<div>
-<a href="#">Facebook</a>
-<a href="#">Twitter</a>
-<a href="#">Instagram</a>
-</div>
-<p>© 2025 Sharvin Sivarajah. All rights reserved.</p>
-</footer>
-`;
-}
+      a {
+        color: #a5d6a7;
+        margin: 0 12px;
+        text-decoration: none;
+        font-weight: 500;
+      }
+
+      a:hover {
+        text-decoration: underline;
+      }
+    `];
+  }
+
+  render() {
+    return html`
+      <footer>
+        <div class="social-links">
+          <a href="https://hax.psu.edu/" target="_blank" rel="noopener noreferrer">
+            Facebook
+          </a>
+          <a href="https://hax.psu.edu/" target="_blank" rel="noopener noreferrer">
+            Twitter
+          </a>
+          <a href="https://hax.psu.edu/" target="_blank" rel="noopener noreferrer">
+            Instagram
+          </a>
+        </div>
+        <p>© 2025 Sharvin Sivarajah. All rights reserved.</p>
+      </footer>
+    `;
+  }
 }
 
 customElements.define(Project2Footer.tag, Project2Footer);
+
